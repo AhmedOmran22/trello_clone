e6101f34181e20b94f32a342f38e235338ccc967
Ahmed Omran <ahmedomran22.dev@gmail.com>
2026-09-04 08:41:21 +0300

    refactor: update comments to clarify the role of refresh tokens in interceptors

--- lib/core/utils/go_router_refresh_stream.dart
+++ lib/core/utils/go_router_refresh_stream.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+I mean it will be a refresh token in the interceptors we do not need a session cubit or how we could handle this ?
 
 /// Bridges a [Stream] (e.g. a Cubit's state stream) to a [Listenable] so
 /// GoRouter can be notified to re-evaluate `redirect` when it emits.
